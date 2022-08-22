d714f5dbb38f360aa876e9c96faecc1f241bbd73
lokadeye <111115136+lokadeye@users.noreply.github.com>
2022-08-22 17:52:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ app_secret = os.environ["APP_SECRET"]
 user_id = os.environ["USER_ID"]
 template_id = os.environ["TEMPLATE_ID"]
 
-def get_date
-  return date['today']
+def get_date():
+  return today
 
 def get_weather():
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
